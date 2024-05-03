c00c84dbf784c864e489eab19dbf23c1cbd32c5a
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-03 15:45:24 +0200

    Done filter

---
+++ components/Search.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const word = ref('');
+const router = useRouter();
+
+const addNew = () => {
+    if (word.value !== "") {
+        router.push(`/${word.value}`);
+    }
+}
+</script>
+
+<template>
+    <input v-model="word" type="text" placeholder="Enter search term" />
+    <button @click="addNew">Search</button>
+</template>
