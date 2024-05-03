8114dc15b646de6b0b76d4a3e8acc8876bde9f1f
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-03 11:49:29 +0200

    modify index

--- components/Search.vue
+++ components/Search.vue
@@ -1,8 +1,22 @@
+<script setup>
+
+const pokemonId = ref(0);
+
+async function submit(){
+
+    const response = await fetch( `https://pokeapi.co/api/v2/pokemon/${pokemonId.value}` )
+    const data = await response.json();
+    console.log(data);
+}
+
+
+</script>
+
 <template>
 
-    <form action="/card">
-        <input type="text" placeholder="Search..">
-        <button type="submit">Search</button>
+    <form @submit.prevent action="/card">
+        <input v-model="pokemonId" type="text" placeholder="Search..">
+        <button @click="submit()">Search</button>
     </form>
 
 </template>
