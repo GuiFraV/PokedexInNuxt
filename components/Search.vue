d4b41d30ed5743c98f4a45ad9175edcedb51d801
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-03 11:34:04 +0200

    Add card pages

--- components/Search.vue
+++ components/Search.vue
@@ -1,8 +1,8 @@
 <template>
 
-    <form>
+    <form action="/card">
         <input type="text" placeholder="Search..">
-        <button type=""submit>Search</button>
+        <button type="submit">Search</button>
     </form>
 
 </template>
