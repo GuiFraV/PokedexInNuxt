618ce92641bd100d13969d674b9f125510dd56b6
FayZerDv <40219975+wfayZerDev@users.noreply.github.com>
2024-05-03 10:33:12 +0200

    Modification du layout, ajout de la navbar.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,39 @@
 <template>
     <div>
-    <h1>Default</h1>
-    <slot />
+        <nav>
+            <nuxt-link to="/">🏠 - Home</nuxt-link> |
+            <nuxt-link to="/search">🔍 - Search</nuxt-link>
+        </nav>
+        <slot />
     </div>
 </template>
+
+<style>
+
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+
+nav {
+    background: #333;
+    padding: 10px;
+    color: white;
+    text-align: center;
+}
+
+nav a {
+    color: white;
+    margin: 0 10px;
+    text-decoration: none;
+    transition: color 0.3s;
+}
+
+nav a:hover {
+    color: lightblue;
+    transition: color 0.3s;
+}
+
+</style>
