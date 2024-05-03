5d89fc69c96d844f858f855445fa31c49f80eaf0
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-03 13:35:10 +0200

    route pokemon done

---
+++ pages/[pokemon].vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const pokemonData = ref(null); // Création d'une variable réactive pour stocker les données du Pokémon
+const { pokemon } = useRoute().params;
+
+async function fetchPokemons() {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const data = await response.json();
+  pokemonData.value = data; 
+
+  console.log(data);
+}
+
+
+fetchPokemons();
+</script>
+
+<template>
+    <div>
+        <p>{{ pokemon }}</p>
+        <ul v-if="pokemonData">
+          <li v-for="ability in pokemonData.abilities" :key="ability.ability.name">
+            {{ ability.ability.name }}
+          </li>
+          <li>
+            HP : {{pokemonData.stats[0].base_stat}}
+          </li>
+          <li>
+            Attack : {{pokemonData.stats[1].base_stat}}
+          </li>
+          <li>
+            defense : {{pokemonData.stats[2].base_stat}}
+          </li>
+          <li>
+            special-attack : {{pokemonData.stats[3].base_stat}}
+          </li>
+          <li>
+            special-defense : {{pokemonData.stats[4].base_stat}}
+          </li>
+          <li>
+            speed : {{pokemonData.stats[5].base_stat}}
+          </li>
+          <li>
+            type : {{pokemonData.types[0].type.name}}
+            {{pokemonData.types[1].type.name}}
+          </li>
+
+        </ul>
+    </div>
+</template>
