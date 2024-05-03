194265d4d320ef8675b82ba8d35b5987da925d2b
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-03 10:56:04 +0200

    fetch 151 pokémons

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-
-
 const { data: data } = await useFetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-
-
 </script>
 
-
 <template>
-     <p v-for="(pokemon, index) in data" :key="index">{{ index }} {{ pokemon }}</p>
+     <p v-for="(pokemon, index) in data.results" :key="index">{{ index }} {{ pokemon.name }}</p>
 </template>
-
-
