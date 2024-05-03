0f4f5598fe9d78fca1f17c4a2f4aa5ed8b566f8c
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-03 17:01:21 +0200

    Components Logic Done

--- components/Search.vue
+++ components/Search.vue
@@ -4,14 +4,22 @@ import { useRouter } from 'vue-router';
 
 const word = ref('');
 const router = useRouter();
+const loading = ref(false); 
 
 const addNew = async () => {
     if (word.value !== "") {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${word.value.toLowerCase()}`);
-        if (response.ok) {
-            router.push(`/${word.value.toLowerCase()}`);
-        } else {
+        loading.value = true; 
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${word.value.toLowerCase()}`);
+            if (response.ok) {
+                router.push(`/${word.value.toLowerCase()}`);
+            } else {
+                router.push('/error');
+            }
+        } catch (error) {
             router.push('/error');
+        } finally {
+            loading.value = false;
         }
     }
 }
@@ -21,6 +29,7 @@ const addNew = async () => {
     <header>
         <input v-model="word" @keyup.enter="addNew" type="text" placeholder="Enter search" />
         <button class="search" @click="addNew">Search</button>
+        <div class="loading" v-if="loading">Loading...</div> 
     </header>
 </template>
 
@@ -40,18 +49,22 @@ input {
     width: 300px;
 }
 
-    .search {
-        padding: 10px;
-        margin: 10px;
-        border-radius: 5px;
-        border: 1px solid #ccc;
-        background-color: #333;
-        color: white;
-        cursor: pointer;
-    }
+.search {
+    padding: 10px;
+    margin: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    background-color: #333;
+    color: white;
+    cursor: pointer;
+}
 
-    .search:hover {
-        background-color: #444;
-    }
+.search:hover {
+    background-color: #444;
+}
+
+.loading{
+    color:white
+}
 
 </style>
