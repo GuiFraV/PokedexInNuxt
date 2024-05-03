3b58b6287db8b3d4af767bae6325ec0c0d3e0c86
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-03 11:05:23 +0200

    Display all images pokémon

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
-const { data: data } = await useFetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+import { onMounted, ref } from 'vue';
+
+const pokemons = ref([]);
+
+async function fetchPokemons() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const data = await response.json();
+  const promises = data.results.map(async (pokemon) => {
+    const response = await fetch(pokemon.url);
+    return await response.json();
+  });
+  pokemons.value = await Promise.all(promises);
+}
+
+onMounted(fetchPokemons);
 </script>
 
 <template>
-     <p v-for="(pokemon, index) in data.results" :key="index">{{ index }} {{ pokemon.name }}</p>
+  <div v-for="(pokemon, index) in pokemons" :key="index">
+    <h2>{{ pokemon.name }}</h2>
+    <img :src="pokemon.sprites.front_default" alt="pokemon.name" />
+    <p>Type: <span v-for="(type, index) in pokemon.types" :key="index">{{ type.type.name }}</span></p>
+  </div>
 </template>
